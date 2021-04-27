2fd4c398c4b6cc3b2a3fe7ed102282032b3225ad
Alexey Tourbin <alexey.tourbin@gmail.com>
2021-04-27 06:28:38 +0300

    decompress.c: initial revision
    This is a starting point, I need it to reproduce some of my findings
    a bit more rigorously.  So this is a pretty basic version which is
    indeed similar to LZ4.  And yet two peculiarities are already evident:
    
    * Minimum match offset is 16.  This means that we can always handle
      a match with SIMD registers.  A small match is copied as 16+2 (while
      in LZ4, a small match is copied as 8+8+2, provided that moff >= 8).
    
    * Match length 19 is not encodable.  In LZ4, match length 19 takes
      an extra "overflow length" byte.  This is strictly worse than encoding
      match length 18 with an extra literal that follows the match, due to a
      branch misprediction.  Moreover, the literal may sometimes be absorbed
      by the next match.
    
    There is no error checking, and we assume that both input and output
    buffers are padded.  For now I concentrate on finding the best sequence
    of instructions for the main loop.
    
    lzbench 1.8 (64-bit Linux)   Assembled by P.Skibinski
    Compressor name         Compress. Decompress. Compr. size  Ratio Filename
    memcpy                   7884 MB/s  7865 MB/s  1227730954 100.00 108 files
    lz4hc 1.9.2 -7             37 MB/s  2771 MB/s   468133763  38.13 108 files
    relz4 0.1                  33 MB/s  3036 MB/s   469379747  38.23 108 files

---
+++ decompress.c
@@ -0,0 +1,55 @@
+#include "xlen.h"
+
+static inline uchar *decompress(const uchar *src, size_t srcSize, uchar *out)
+{
+    const uchar *srcLast5 = src + srcSize - 5;
+    while (1) {
+	uint llen = *src++;
+	uint mlen = llen;
+	llen >>= 4;
+	if (likely(mlen < 0xf0)) {
+	    memcpy(out, src, 16);
+	    src += llen, out += llen;
+	}
+	else {
+	    llen += getxlen(&src);
+	    uchar *outEnd = out + llen;
+	    do {
+		memcpy(out, src, 32);
+		src += 32, out += 32;
+	    } while (out < outEnd);
+	    src -= (out - outEnd);
+	    out = outEnd;
+	}
+	if (unlikely(src > srcLast5))
+	    break;
+	uint moff = load16le(src);
+	mlen &= 15;
+	const uchar *ref = out - moff;
+	if (likely(mlen != 15)) {
+	    memcpy(out + 0, ref - 16, 16);
+	    src += 2;
+	    memcpy(out + 16, ref + 0, 2);
+	    mlen += 4;
+	    out += mlen;
+	}
+	else {
+	    src += 2;
+	    mlen = 20 + getxlen(&src);
+	    uchar *outEnd = out + mlen;
+	    do {
+		memcpy(out + 0, ref - 16, 16);
+		memcpy(out + 16, ref + 0, 16);
+		ref += 32, out += 32;
+	    } while (out < outEnd);
+	    out = outEnd;
+	}
+    }
+    assert(src == srcLast5 + 5);
+    return out;
+}
+
+size_t RELZ4_decompress(const void *src, size_t srcSize, void *out)
+{
+    return decompress(src, srcSize, out) - (const uchar *) out;
+}
