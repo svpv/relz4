69a21ba2c53536c5c8e49531c74855d5fbb5a204
Alexey Tourbin <alexey.tourbin@gmail.com>
2021-06-15 03:39:18 +0300

    ht.c: only merge M+M2 into L+M2 if mlen2 <= OPTMLEN
    When mlen2 <= OPTMLEN, we know that moving the boundary between M and M2
    to the right cannot save us an extra mlen byte on behalf of M2.
    
    -482392848 compressed bytes
    +482259341 compressed bytes
    
    -7217309/77720388 tokens
    +7105392/78063879 tokens

--- ht.c
+++ ht.c
@@ -197,7 +197,8 @@ static uchar *HT_compress(const uchar *src, size_t srcSize, uchar *out)
 	}
 	if (unlikely(mstart0 < mstart) && likely(mstart2 < mstart + mlen0))
 	    mstart = mstart0, moff = moff0, mlen = mlen0;
-	if (likely(mstart2 - mstart < 3) || (mstart2 - mstart == 3 &&
+	if (likely(mstart2 - mstart < 3) ||
+		(mstart2 - mstart == 3 && mlen2 <= OPTMLEN &&
 		    !(mstart - src0 <= OPTLLEN && mstart2 - src0 > OPTLLEN))) {
 	    mstart = mstart2, moff = moff2, mlen = mlen2;
 	    src = mstart + mlen - 3;
